b5861a191b9191842b69055b179605f276a5a772
Paulo P <pssp25@hotmail.com>
2025-08-27 16:05:44 +0200

    Update hierarchy

--- Scripts/calculate_hierarchy.py
+++ Scripts/calculate_hierarchy.py
@@ -1,37 +1,37 @@
 import pandas as pd
 
-print("This is a placeholder for the fix_hierarchy.py script.")
+pd.set_option('future.no_silent_downcasting', True)
 
+print("Fixing hierarchy...")
 
+last_col_name = "result"
 csv_file = "data.csv"
 df = pd.read_csv(csv_file)
+new_df = pd.DataFrame(columns=df.columns)
 
-
-# Iterate df
 keysx = [ -1 ]
 i = 0
 for index, row in df.iterrows():
-    if not pd.isna(row['result']):
+    if not pd.isna(row[last_col_name]):
         keysx.append(i)
     i = i + 1
 
 for x in range(len(keysx)-1):
-    df.iloc[keysx[x]+1:keysx[x+1]+1, :-1] = df.iloc[keysx[x]+1:keysx[x+1]+1, :-1].ffill()
+    df.iloc[keysx[x]+1:keysx[x+1], :-1] = df.iloc[keysx[x]+1:keysx[x+1], :-1].ffill()
 
 
+keysn = [x + 1 for x in keysx]
+keysn.reverse()
 
 
 def get_range(text):
-    # replace [ and ] with empty string
     try:
         text = text.replace("[", "").replace("]", "")
     except Exception as e:
-        # print(f"Error processing text: {e}")
         return []
 
     if '-' in text:
         start, end = text.split('-')
-        # check if the first character is a letter i.e. A-Z
         if start[0].isalpha() and end[0].isalpha():
             prefix = start[0]
             start_num = int(start[1:])
@@ -42,35 +42,34 @@ def get_range(text):
         return [text]
 
 
+# create a new df with same columns as df
+def apply_results(res_values, from_value, to_value):
+    res = pd.DataFrame(columns=df.columns)
+    for i in range(from_value, to_value-1):
+        for val in res_values:
+            new_row = df.iloc[i].copy()
+            new_row[last_col_name] = val
+            res = pd.concat([res, new_row.to_frame().T], ignore_index=True)
+            print(new_row)
+    return res
 
-new_rows = []
-for index, row in df.iterrows():
-    if not pd.isna(row['result']):
-        # if contains - split into and add values into new_rows
-        new_rows.extend(get_range(row['result']))
+
+# iterate keysn
+results = []
+for x in range(len(keysn)-1):
+    if (keysn[x] - keysn[x+1]) == 1:       
+        c = df.at[keysn[x+1], last_col_name]
+        results.extend(get_range(c))
     else:
-        # else add the value as is
-        new_rows.append(row['result'])
-    i = i + 1
+        if (x > 0):
+            c = df.at[keysn[x]-1, last_col_name]
+            results.extend(get_range(c))        
+        res1 = apply_results(results, keysn[x+1], keysn[x])
 
+        new_df = pd.concat([new_df, res1], ignore_index=True)
+        results = []
 
-
-# New DataFrame
-new_df = pd.DataFrame(columns=df.columns)
-for index, row in df.iterrows():
-    new_rows = get_range(row['result'])
-    # while next row['result'] is not nan
-    # while index + 1 < len(df) and not pd.isna(df.iloc[index + 1]['result']):
-    #     new_rows.extend(get_range(df.iloc[index + 1]['result']))
-    #     index += 1
-
-    for new_row in new_rows:
-        dup_row = row[:-1]
-        dup_row['result'] = new_row
-        new_df = pd.concat([new_df, dup_row.to_frame().T], ignore_index=True)
-
-
-print("----")
+# Result information
+new_df.to_csv("new_data.csv", index=False)
 print(new_df)
 
-# new_df.to_csv("new_data.csv", index=False)
